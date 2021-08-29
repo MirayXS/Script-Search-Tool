2d5597eb8c4b9c6f6d3c640011ab6694e1dad4a4
Nebylax <admin@latae.xyz>
2021-08-28 22:01:03 -0500

    Update ScriptSearchToolLoadstring.lua

--- ScriptSearchToolLoadstring.lua
+++ ScriptSearchToolLoadstring.lua
@@ -1,3 +1,3 @@
 -- Script Search Tool | reggie#1000 --
 
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/Nextrixcs/Script-Search-Tool/main/ScriptSearchToolLoadstring.lua'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Nextrixcs/Script-Search-Tool/main/Script%20Search%20Tool.lua'),true))()
