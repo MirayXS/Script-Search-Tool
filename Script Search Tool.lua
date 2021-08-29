ae628bb84b192ae6116acd26ef57a6f7b607097e
reggie <admin@latae.xyz>
2021-08-29 10:35:49 -0500

    fixed clear button not cancelling search

--- Script Search Tool.lua
+++ Script Search Tool.lua
@@ -903,8 +903,10 @@ end
 	ScriptSearchTool.Status.Text = 'Scripts Decompiled: 0/'..#ScriptsInGame
 	ScriptSearchTool.Start.Visible = false
 	for i,v in pairs(ScriptsInGame) do
-		if v ~= nil then
+		if v ~= nil and SearchCancelled == false then
 			AddResult(v)
+		else
+			break
 		end
 	end
 end)
